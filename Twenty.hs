dd8729fd73e61ca4d532207b37ccb60c8c0d3519
RJ Regenold <rjregenold@gmail.com>
2013-05-04 15:02:42 -0500

    Whoops. Lost the class in there somewhere.

--- Twenty.hs
+++ Twenty.hs
@@ -1,6 +1,9 @@
 -- From Tony's Blog
 -- http://blog.tmorris.net/posts/20-intermediate-haskell-exercises/
 
+class Fluffy f where
+  furry :: (a -> b) -> f a -> f b
+
 -- Exercise 1
 -- Relative Difficulty: 1
 instance Fluffy [] where
